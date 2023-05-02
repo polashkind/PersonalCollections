91bff3c70559b6127878bd314bd6d19d47780f7c
Polina Marinovich <polashkind@gmail.com>
2023-05-02 21:42:47 +0300

    create controllers for models

---
+++ PersonalCollections/Controllers/CollectionsController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalCollections.Data;
+
+namespace PersonalCollections.Controllers
+{
+    public class CollectionsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CollectionsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allCollections = await _context.Collections.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ PersonalCollections/Controllers/ItemsController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PersonalCollections.Data;
+
+namespace PersonalCollections.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ItemsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allItems = await _context.Items.ToListAsync();
+            return View();
+        }
+    }
+}
