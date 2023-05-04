18324618b28ac294ebb5b01cb4392845864fdb22
Polina Marinovich <polashkind@gmail.com>
2023-05-04 11:22:03 +0300

    add description names to models

--- PersonalCollections/Models/Collection.cs
+++ PersonalCollections/Models/Collection.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
 
 namespace PersonalCollections.Models
 {
-	public class Collection
+    public class Collection
 	{
         public int Id { get; set; }
+
+        [Display(Name = "Title")]
         public string Title { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
+        [Display(Name = "Subject")]
         public string Subject { get; set; }
 
         public List<Item> Items { get; set; }
--- PersonalCollections/Models/Item.cs
+++ PersonalCollections/Models/Item.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PersonalCollections.Models
@@ -6,9 +7,17 @@ namespace PersonalCollections.Models
 	public class Item
 	{
 		public int Id { get; set; }
+
+        [Display(Name = "Title")]
         public string Title { get; set; }
+
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
+        [Display(Name = "Created")]
         public DateTime CreatedAt { get; set; }
+
+        [Display(Name = "Likes")]
         public int Likes { get; set; }
 
         public int CollectionId { get; set; }
